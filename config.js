52f9492ff556f2d9108757d842ff478eaadd0288
murakamidaisuke <Koubou.hi.rx7@gmail.com>
2026-01-19 10:34:02 +0900

    Update Supabase configuration

--- config.js
+++ config.js
@@ -8,8 +8,8 @@
 const CRM_CONFIG = {
     // Supabase Configuration (Sync)
     supabase: {
-        url: '', // e.g. https://xyz.supabase.co
-        key: ''  // e.g. eyJhbG...
+        url: 'https://isoqzliuqtgobipilioh.supabase.co', // e.g.https://isoqzliuqtgobipilioh.supabase.co
+        key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlzb3F6bGl1cXRnb2JpcGlsaW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzNzE1ODMsImV4cCI6MjA4Mzk0NzU4M30.nKG60pQ0b90paMpwrNfhbB__RnlNxglvOtqmo3ztE8c'  // e.g. eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlzb3F6bGl1cXRnb2JpcGlsaW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzNzE1ODMsImV4cCI6MjA4Mzk0NzU4M30.nKG60pQ0b90paMpwrNfhbB__RnlNxglvOtqmo3ztE8c
     },
 
     // Google Services
